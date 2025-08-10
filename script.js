9c95bcf8dc26c65a38b0448b005b21dda664403a
manyadavesar10 <manyadavesar@gmail.com>
2025-08-10 23:22:11 +0530

    added logic to show X or O

--- script.js
+++ script.js
@@ -14,3 +14,14 @@ const winPatterns = [
     [6,7,8]
 ];
 
+boxes.forEach((box) =>{
+    box.addEventListener("click", () => {
+        if(turnO) {
+            box.innerText="O";
+            turnO = false;
+        } else{
+            box.innerText="X";
+            turnO = false;
+        }
+    })
+})
